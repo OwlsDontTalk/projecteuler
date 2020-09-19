826c556574e24022783fd95ab3f2cc666887c77a
Ivan Petrov <petrovodt@gmail.com>
2020-09-19 15:11:44 +0300

    2-3 resolved, 4in process

--- src/main/java/SolutionExecutor.java
+++ src/main/java/SolutionExecutor.java
@@ -1,5 +1,7 @@
 public class SolutionExecutor {
     public static void main(String[] args) {
-        Problem1.calculate();
+        Problem1to10.problem1();
+        Problem1to10.problem2();
+        Problem1to10.problem3(600851475143L);
     }
 }
