36b493bacfab511fea630b21b36b1eefa8e4ed55
Ivan Petrov <petrovodt@gmail.com>
2020-09-25 17:18:22 +0300

    problem 8 init

--- src/main/java/SolutionExecutor.java
+++ src/main/java/SolutionExecutor.java
@@ -9,6 +9,7 @@ public class SolutionExecutor {
         //Problem1to10.problem5(20);
         //Problem1to10.problem6();
         //Problem1to10.largestPrimeFactor(14l);
-        Problem1to10.findNprimeNumber(10001);
+        //Problem1to10.findNprimeNumber(10001);
+        Problem1to10.adjacentDigitsMultiply(4);
     }
 }
